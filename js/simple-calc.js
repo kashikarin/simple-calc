a5779f99c11a6d0fa9d736140d9f394082649a63
kashikarin <karinkashi@gmail.com>
2025-05-21 13:37:22 +0300

    Implement num and operator handlers

--- js/simple-calc.js
+++ js/simple-calc.js
@@ -1,11 +1,16 @@
 'use strict'
 
-function onNumClick(cell){
-    console.log('hi')
+var num1 = 0
+var num2 = 0
+var operator = ''
+var x = 10
+function onNumClick(elCell){
+    if (!num1) num1 = +elCell.innerText
+    else if (operator && !num2) num2 = +elCell.innerText
 }
 
-function onOperatorClick(Cell){
-    console.log('hi')
+function onOperatorClick(elCell){
+    if (num1 && !num2) operator = elCell.innerText
 }
 
 function onCalc(){
